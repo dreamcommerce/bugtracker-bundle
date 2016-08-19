2fa3f98c02377953e219e0fe827dedf05024f04b
kotarb <konrad.kotarba@dreamcommerce.com>
2016-08-19 14:04:16 +0200

    Update DreamCommerceBugTrackerBundle.php

--- DreamCommerceBugTrackerBundle.php
+++ DreamCommerceBugTrackerBundle.php
@@ -6,4 +6,9 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class DreamCommerceBugTrackerBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $container->addCompilerPass(new CollectorCompilerPass(), PassConfig::TYPE_BEFORE_REMOVING);
+    }
 }
